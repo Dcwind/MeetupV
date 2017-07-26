be0b9be43b5a126a780195d52a8eede5e452d79a
decent <danielekwuazi@gmail.com>
2017-07-26 20:05:03 +0700

    steamlining boilerplate

--- src/App.vue
+++ src/App.vue
@@ -8,16 +8,7 @@
   export default {
     data () {
       return {
-        clipped: false,
-        drawer: true,
-        fixed: false,
-        items: [
-          { icon: 'bubble_chart', title: 'Inspire' }
-        ],
-        miniVariant: false,
-        right: true,
-        rightDrawer: false,
-        title: 'Vuetify.js'
+      
       }
     }
   }
