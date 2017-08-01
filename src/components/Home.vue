dcdd9c646d35edbe2f5452f0ea19e033e048017b
decent <danielekwuazi@gmail.com>
2017-08-01 22:52:42 +0700

    centralized buttons

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,10 +1,10 @@
 <template>
    <v-container>
        <v-layout wrap row>
-           <v-flex xs12 sm6 >
+           <v-flex xs12 sm6 class="text-xs-center text-sm-right">
                <v-btn large route to="/meetups">View Meetups</v-btn>
            </v-flex>
-           <v-flex xs12 sm6 >
+           <v-flex xs12 sm6 class="text-xs-center text-sm-right">
                <v-btn large route to="/meetup/createnew">Organize Meetup</v-btn>
            </v-flex>
        </v-layout>
