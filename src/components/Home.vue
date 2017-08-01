57dfb49a960d1e55e1656ce38fcd548c508bc34d
decent <danielekwuazi@gmail.com>
2017-08-01 22:43:17 +0700

    setting up layout

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,10 @@
 <template>
    <v-container>
-       
+       <v-layout wrap row>
+           <v-flex xs12 sm6 >
+               <v-btn></v-btn>
+           </v-flex>
+       </v-layout>
    </v-container>
 </template>
 
