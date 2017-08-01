dbf9cdfc337be0e5ae0ed0636722bf0df28a17f9
decent <danielekwuazi@gmail.com>
2017-08-01 22:49:22 +0700

    buttons

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,7 +2,10 @@
    <v-container>
        <v-layout wrap row>
            <v-flex xs12 sm6 >
-               <v-btn></v-btn>
+               <v-btn large route to="/meetups">View Meetups</v-btn>
+           </v-flex>
+           <v-flex xs12 sm6 >
+               <v-btn large route to="/meetup/createnew">Organize Meetup</v-btn>
            </v-flex>
        </v-layout>
    </v-container>
