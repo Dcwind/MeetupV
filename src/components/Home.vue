15ac24d0495baccff0b9f6e88634e48caa644c58
decent <danielekwuazi@gmail.com>
2017-08-02 20:28:23 +0700

    carousel in progress

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -8,6 +8,11 @@
                <v-btn large route to="/meetup/createnew">Organize Meetup</v-btn>
            </v-flex>
        </v-layout>
+       <v-layout row wrap>
+           <v-carousel>
+              <v-carousel-item v-for="(item,i) in items" v-bind:src="item.src" :key="i"></v-carousel-item>
+           </v-carousel>
+       </v-layout>
    </v-container>
 </template>
 
