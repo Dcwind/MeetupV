d2b65eee4604ab285ae212c4d5ccf75c0a01c42d
decent <danielekwuazi@gmail.com>
2017-08-02 22:40:48 +0700

    vuetify spacing

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -4,24 +4,29 @@
            <v-flex xs12 sm6 class="text-xs-center text-sm-right">
                <v-btn large router to="/meetups">View Meetups</v-btn>
            </v-flex>
-           <v-flex xs12 sm6 class="text-xs-center text-sm-right">
+           <v-flex xs12 sm6 class="text-xs-center text-sm-left">
                <v-btn large router to="/meetup/createnew">Organize Meetup</v-btn>
            </v-flex>
        </v-layout>
-       <v-layout row wrap>
+       <v-layout row wrap class="mt-2">
          <v-flex xs12>
            <v-carousel>
               <v-carousel-item 
-              v-for="meetup in meetups" 
-              :src="meetup.imageUrl" 
-              :key="meetup.id">
-              <div class="title">
-                {{ meetup.title  }}  
-              </div>
-              </v-carousel-item>
-              
+                v-for="meetup in meetups" 
+                :src="meetup.imageUrl" 
+                :key="meetup.id">
+
+                <div class="title">
+                    {{ meetup.title  }}  
+                </div>
+              </v-carousel-item>   
            </v-carousel>
          </v-flex>
+       </v-layout>
+       <v-layout row wrap class="mt-2">
+        <v-flex xs12 >
+            <p>Join our awesome meetup</p>
+        </v-flex>
        </v-layout>
    </v-container>
 </template>
@@ -41,6 +46,11 @@
 
 <style scoped>
 .title {
-
+position: absolute;
+bottom: 50px;
+background-color: rgba(0,0,0,0.5);
+color: white;
+padding: 10px;
+font-size: 2em;
 }
 </style>
