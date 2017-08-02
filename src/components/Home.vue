3b4815daa7e6fb240fe3541b7261103ecd2a1a9e
decent <danielekwuazi@gmail.com>
2017-08-02 22:19:26 +0700

    error - Property or method "meetup" is not defined

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,20 +2,21 @@
    <v-container>
        <v-layout wrap row>
            <v-flex xs12 sm6 class="text-xs-center text-sm-right">
-               <v-btn large route to="/meetups">View Meetups</v-btn>
+               <v-btn large router to="/meetups">View Meetups</v-btn>
            </v-flex>
            <v-flex xs12 sm6 class="text-xs-center text-sm-right">
-               <v-btn large route to="/meetup/createnew">Organize Meetup</v-btn>
+               <v-btn large router to="/meetup/createnew">Organize Meetup</v-btn>
            </v-flex>
        </v-layout>
        <v-layout row wrap>
          <v-flex xs12>
            <v-carousel>
-              <v-carousel-item v-for="meetup in meetups" 
+              <v-carousel-item 
+              v-for="meetup in meetups" 
               :src="meetup.imageUrl" 
               :key="meetup.id"></v-carousel-item>
               <div class="title">
-                {{ meetup.title }}
+                {{ meetup.title  }}  
               </div>
            </v-carousel>
          </v-flex>
@@ -24,18 +25,16 @@
 </template>
 
 <script>
-    export default {
-        data(){
-            return {
-                meetups: [
-                    {imageUrl: 'http://dailysignal.com/wp-content/uploads/150904_CaliforniaMigration_Johnson-1250x650.jpg',
-                     id: 'dfkjdfkjdkj', title: 'Meetup in Cali'},
-                    {imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/f/f6/Bangkok_skytrain_sunset.jpg',
-                     id: 'sfdvfger3geg', title: 'Meetup in Bangkok'}
-                ]
-            }
-        }
+  export default {
+    data () {
+      return {
+        meetups: [
+          {imageUrl: 'http://dailysignal.com/wp-content/uploads/150904_CaliforniaMigration_Johnson-1250x650.jpg', id: 'dfkjdfkjdkj', title: 'Meetup in Cali'},
+          {imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/f/f6/Bangkok_skytrain_sunset.jpg', id: 'sfdvfger3geg', title: 'Meetup in Bangkok'}
+        ]
+      }
     }
+  }
 </script>
 
 <style scoped>
