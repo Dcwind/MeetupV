494bd58f0b6b51e1483bf631a4c0a9fd35b3075d
decent <danielekwuazi@gmail.com>
2017-08-02 20:56:38 +0700

    assign id

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -11,9 +11,9 @@
        <v-layout row wrap>
          <v-flex xs12>
            <v-carousel>
-              <v-carousel-item v-for="(item,i) in items" 
-              v-bind:src="item.src" 
-              :key="i"></v-carousel-item>
+              <v-carousel-item v-for="meetup in meetups" 
+              v-bind:src="meetup.imageUrl" 
+              :key="meetup.id"></v-carousel-item>
            </v-carousel>
          </v-flex>
        </v-layout>
@@ -25,7 +25,10 @@
         data(){
             return {
                 meetups: [
-                    {}
+                    {imageUrl: 'http://dailysignal.com/wp-content/uploads/150904_CaliforniaMigration_Johnson-1250x650.jpg',
+                     id: 'dfkjdfkjdkj', title: 'Meetup in Cali'},
+                    {imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/f/f6/Bangkok_skytrain_sunset.jpg',
+                     id: 'sfdvfger3geg', title: 'Meetup in Bangkok'}
                 ]
             }
         }
