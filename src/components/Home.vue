4a6c54fb2a232c2ea733f35f6a2300f33db211ab
decent <danielekwuazi@gmail.com>
2017-08-02 20:41:50 +0700

    data

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -11,10 +11,23 @@
        <v-layout row wrap>
          <v-flex xs12>
            <v-carousel>
-              <v-carousel-item v-for="(item,i) in items" v-bind:src="item.src" :key="i"></v-carousel-item>
+              <v-carousel-item v-for="(item,i) in items" 
+              v-bind:src="item.src" 
+              :key="i"></v-carousel-item>
            </v-carousel>
          </v-flex>
        </v-layout>
    </v-container>
 </template>
 
+<script>
+    export defualt {
+        data(){
+            return {
+                meetups: [
+                    {}
+                ]
+            }
+        }
+    }
+</script>
