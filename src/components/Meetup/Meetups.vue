064ff1ed75e4373919e718d5ed7a61683dff5ad1
decent <danielekwuazi@gmail.com>
2017-08-03 16:08:06 +0700

    added card media

--- src/components/Meetup/Meetups.vue
+++ src/components/Meetup/Meetups.vue
@@ -5,7 +5,10 @@
             <v-card>
                 <v-container fluid>
                     <v-layout row>
-                        <v-flex xs5>
+                            <v-flex xs5 sm4 md3>
+                                <v-card-media src="https://upload.wikimedia.org/wikipedia/commons/f/f6/Bangkok_skytrain_sunset.jpg" 
+                                height="130px">
+                            </v-card-media>
                         </v-flex>
                     </v-layout>
                 </v-container>
