a39417b2127828821180d2552264b0c1f94a23d1
decent <danielekwuazi@gmail.com>
2017-08-03 15:57:31 +0700

    in progress

--- src/components/Meetup/Meetups.vue
+++ src/components/Meetup/Meetups.vue
@@ -2,6 +2,14 @@
    <v-container>
     <v-layout row wrap>
         <v-flex x12>
+            <v-card>
+                <v-container fluid>
+                    <v-layout row>
+                        <v-flex xs5>
+                        </v-flex>
+                    </v-layout>
+                </v-container>
+            </v-card>
         </v-flex>
     </v-layout>
    </v-container>
