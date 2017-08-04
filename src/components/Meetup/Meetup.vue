15b8b39498aec2c1b576d33263dace1d671af456
decent <danielekwuazi@gmail.com>
2017-08-04 16:18:29 +0700

    in progress

--- src/components/Meetup/Meetup.vue
+++ src/components/Meetup/Meetup.vue
@@ -1,7 +1,13 @@
 <template>
     <v-container>
-        <v-layout row>
-            <v-flex>
+        <v-layout row wrap>
+            <v-flex xs12>
+                <v-card>
+                    <v-card-title><h3>My Meetup</h3></v-card-title>
+                    <v-card-media src="https://upload.wikimedia.org/wikipedia/commons/f/f6/Bangkok_skytrain_sunset.jpg" 
+                     height="130px">
+                    </v-card-media>
+                </v-card>
             </v-flex>
         </v-layout>
     </v-container>
