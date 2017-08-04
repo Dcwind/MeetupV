0f0e0a48c6e82e9d1c365c3aeb73cb20705762e5
decent <danielekwuazi@gmail.com>
2017-08-04 16:52:05 +0700

    card text ans actions

--- src/components/Meetup/Meetup.vue
+++ src/components/Meetup/Meetup.vue
@@ -3,10 +3,22 @@
         <v-layout row wrap>
             <v-flex xs12>
                 <v-card>
-                    <v-card-title><h3>My Meetup</h3></v-card-title>
+                    <v-card-title><h5>My Meetup</h5></v-card-title>
                     <v-card-media src="https://upload.wikimedia.org/wikipedia/commons/f/f6/Bangkok_skytrain_sunset.jpg" 
-                     height="130px">
+                     height="400px">
                     </v-card-media>
+                    <v-card-text>
+                    <div class="info--text">27th August 2018 - Location</div>
+                    <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                    Eum sequi quae qui, reiciendis sit recusandae magni similique fugit 
+                    sed repellendus esse earum voluptatum illo, unde dolorum quia alias
+                     at aspernatur.
+                     </div>
+                    </v-card-text>
+                    <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn>Register</v-btn>
+                    </v-card-actions>
                 </v-card>
             </v-flex>
         </v-layout>
