83f04f5ced9a0cbe7250d277eda3c4011e276ede
decent <danielekwuazi@gmail.com>
2017-08-05 22:03:29 +0700

    loadedMeetups from store

--- src/components/Meetup/Meetups.vue
+++ src/components/Meetup/Meetups.vue
@@ -2,23 +2,23 @@
    <v-container>
     <v-layout row wrap>
         <v-flex x12>
-            <v-card>
+            <v-card v-for="meetup in meetups" :key="meetup.id">
                 <v-container fluid>
                     <v-layout row>
                         <v-flex xs5 sm4 md3>
-                            <v-card-media src="https://upload.wikimedia.org/wikipedia/commons/f/f6/Bangkok_skytrain_sunset.jpg" 
+                            <v-card-media :src="meetup.imageUrl" 
                                 height="130px">
                             </v-card-media>
                         </v-flex>
                         <v-flex xs7 sm8 md9>
                             <v-card-title primary-title>
                                 <div>
-                                    <h5 class="mb-0">Bangkok Meetup</h5>
-                                    <div >27th August 2018</div>
+                                    <h5 class="mb-0">{{meetup.title}}</h5>
+                                    <div >{{meetup.date}}</div>
                                 </div>
                             </v-card-title>
                             <v-card-actions>
-                                <v-btn to="/meetup/1"> 
+                                <v-btn :to=" '/meetup/' + meetup.id"> 
                                 <v-icon left>arrow_forward</v-icon>
                                 View Meetup
                                 </v-btn>
