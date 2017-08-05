0ed3c8da842311d14011500d8cdcd4b36ecad736
decent <danielekwuazi@gmail.com>
2017-08-05 20:03:07 +0700

    in progress

--- src/store/index.js
+++ src/store/index.js
@@ -5,11 +5,32 @@ Vue.use(Vuex)
 
 export const store = new Vuex.Store({
   state: {
-    LoadedMeetups: [
-        
-    ]
+    loadedMeetups: [
+      {
+        imageUrl: 'http://dailysignal.com/wp-content/uploads/150904_CaliforniaMigration_Johnson-1250x650.jpg',
+        id: 'dfkjdfkjdkj',
+        title: 'Meetup in Cali',
+        date: '2017-08-23'
+      },
+      { imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/f/f6/Bangkok_skytrain_sunset.jpg',
+        id: 'sfdvfger3geg',
+        title: 'Meetup in Bangkok',
+        date: '2017-09-23'
+      }
+    ],
+    user: {
+      id: 'mfksdmkf32mk',
+      registeredMeetups: [
+        'sfdvfger3geg'
+      ]
+    }
   },
   mutations: {},
   actions: {},
-  getters: {}
+  getters: {
+    loadedMeetups (state) {
+        return state.loadedMeetups
+    }
+  }
 })
+
