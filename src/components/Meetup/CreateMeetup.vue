8025c0d90384d95d0cff71617e6fe957ccbe83f9
decent <danielekwuazi@gmail.com>
2017-08-06 09:36:00 +0700

    style preview

--- src/components/Meetup/CreateMeetup.vue
+++ src/components/Meetup/CreateMeetup.vue
@@ -40,7 +40,7 @@
                     </v-layout>
                     <v-layout row>
                         <v-flex xs12 sm6 offset-sm3>
-                          <img :src="imageUrl" >
+                          <img :src="imageUrl" height="150">
                         </v-flex>
                     </v-layout>
                     <v-layout row>
