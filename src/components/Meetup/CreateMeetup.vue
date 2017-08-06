f9c0330d18d102190ccb12f18f11cb482dd6565b
decent <danielekwuazi@gmail.com>
2017-08-06 08:23:37 +0700

    in progress

--- src/components/Meetup/CreateMeetup.vue
+++ src/components/Meetup/CreateMeetup.vue
@@ -1,7 +1,19 @@
 <template>
     <v-container>
         <v-layout row>
-            <v-flex xs12>
+            <v-flex xs12 sm6 offset-sm3>
+                <h4>Create a new meetup</h4>
+            </v-flex>
+        </v-layout>
+        <v-layout row>
+            <v-flex xs12 >
+                <form>
+                    <v-layout>
+                        <v-flex xs12 sm6 offset-sm3>
+
+                        </v-flex>
+                    </v-layout>
+                </form>
             </v-flex>
         </v-layout>
     </v-container>
