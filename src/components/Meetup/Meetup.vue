3f10e59652a53732e2ffaabdffe03e19d7cb2042
decent <danielekwuazi@gmail.com>
2017-08-08 20:09:30 +0700

    call the date filter

--- src/components/Meetup/Meetup.vue
+++ src/components/Meetup/Meetup.vue
@@ -8,7 +8,7 @@
                      height="400px">
                     </v-card-media>
                     <v-card-text>
-                    <div class="info--text">{{meetup.date}} - {{meetup.location}}</div>
+                    <div class="info--text">{{meetup.date | date}} - {{meetup.location}}</div>
                     <div>{{meetup.description}}
                      </div>
                     </v-card-text>
