cc80293aa56a642e9294c989a5202cdb00efb0a4
decent <danielekwuazi@gmail.com>
2017-08-09 17:45:46 +0700

    signUp form in progress

--- src/components/User/SignUp.vue
+++ src/components/User/SignUp.vue
@@ -1,5 +1,8 @@
 <template>
-    <div>
-        <p>The Signup page</p>
-    </div>
+    <v-container>
+        <v-layout row>
+            <v-flex xs12 sm6 offset-sm3>
+            </v-flex>
+        </v-layout>
+    </v-container>
 </template>
