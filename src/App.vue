6bb2adc6f899a0ecf42ea616b1e2e590c2cc961b
decent <danielekwuazi@gmail.com>
2017-08-12 18:59:36 +0700

    menu on authentication in progress

--- src/App.vue
+++ src/App.vue
@@ -42,15 +42,25 @@
   export default {
     data () {
       return {
-        sideNav: false,
-        menuItems: [
-          { icon: 'chrome_reader_mode', title: 'View Meetups', link: '/meetups' },
-          { icon: 'room', title: 'Organize Meetups', link: '/meetup/createnew' },
-          { icon: 'person', title: 'Profile', link: '/profile' },
+        sideNav: false
+      }
+    },
+    computed: {
+      menuItems () {
+        let menuItems = [
           { icon: 'face', title: 'Sign up', link: '/signup' },
           { icon: 'lock_open', title: 'Sign in', link: 'signin' }
         ]
-      }
+        if () {
+          menuItems = [
+          { icon: 'chrome_reader_mode', title: 'View Meetups', link: '/meetups' },
+          { icon: 'room', title: 'Organize Meetups', link: '/meetup/createnew' },
+          { icon: 'person', title: 'Profile', link: '/profile' }
+        ]
+        return menuItems
+        }
+      },
+      
     }
   }
 </script>
