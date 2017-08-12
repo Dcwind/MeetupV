48c19b6ab4f0f48677af4a042056eda25bafdf94
decent <danielekwuazi@gmail.com>
2017-08-12 19:11:11 +0700

    signin component in progress

--- src/components/User/SignIn.vue
+++ src/components/User/SignIn.vue
@@ -1,5 +1,77 @@
 <template>
-    <div>
-        <p>The SignIn page</p>
-    </div>
+    <v-container>
+        <v-layout row>
+            <v-flex xs12 sm6 offset-sm3>
+                <v-card>
+                    <v-card-text>
+                        <v-container>
+                            <form @submit.prevent="onSignIn">
+                                <v-layout row>
+                                    <v-flex >
+                                        <v-text-field
+                                        name="email"
+                                        label="Mail"
+                                        id="email"
+                                        type="email"
+                                        v-model="email"
+                                        required>
+                                        </v-text-field>
+                                    </v-flex>
+                                </v-layout>
+                                <v-layout row>
+                                    <v-flex >
+                                        <v-text-field
+                                        name="password"
+                                        label="Password"
+                                        id="password"
+                                        type="password"
+                                        v-model="password"
+                                        required>
+                                        </v-text-field>
+                                    </v-flex>
+                                </v-layout>
+                              <v-layout row>
+                                  <v-flex >
+                                      <v-btn type="submit" >Sign Up</v-btn>
+                                  </v-flex>
+                              </v-layout>
+                            </form>
+                        </v-container>
+                    </v-card-text>
+                </v-card>
+            </v-flex>
+        </v-layout>
+    </v-container>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      email: '',
+      password: '',
+      comfirmPassword: ''
+    }
+  },
+  computed: {
+    comparePassword () {
+      return this.password !== this.comfirmPassword ? 'Passwords do not match' : true
+    },
+    user () {
+      return this.$store.getters.user
+    }
+  },
+  watch: {
+    user (value) {
+      if (value !== null && value !== undefined) {
+        this.$router.push('/')
+      }
+    }
+  },
+  methods: {
+    onSignUp () {
+      this.$store.dispatch('signUserUp', {email: this.email, password: this.password})
+    }
+  }
+}
+</script>
