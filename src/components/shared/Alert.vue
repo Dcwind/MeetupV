f3f7915f5b911a3d9735a2e7a49141637202331e
decent <danielekwuazi@gmail.com>
2017-08-17 17:12:23 +0700

    on close method

---
+++ src/components/shared/Alert.vue
@@ -0,0 +1,15 @@
+<template>
+<v-alert error dismissible v-model="alert">
+  This is a success alert that is closable.
+</v-alert>
+</template>
+
+<script>
+export default {
+  methods: {
+    onClose () {
+      this.$emit('dismissed')
+    }
+  }
+}
+</script>
