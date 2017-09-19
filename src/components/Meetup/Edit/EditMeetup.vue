799e0ab162ac2b5a99737f4e96285756783d00bd
decent <danielekwuazi@gmail.com>
2017-09-19 22:03:17 +0700

    dialog content

--- src/components/Meetup/Edit/EditMeetup.vue
+++ src/components/Meetup/Edit/EditMeetup.vue
@@ -3,5 +3,13 @@
         <v-btn fab accent slot="activator">
             <v-icon>edit</v-icon>
         </v-btn>
+        <v-card>
+            <v-container>
+                <v-layout row wrap>
+                    <v-flex xs12>
+                    </v-flex>
+                </v-layout>
+            </v-container>
+        </v-card>
     </v-dialog>
 </template>
