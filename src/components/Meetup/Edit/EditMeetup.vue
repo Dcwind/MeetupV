d5532a33e11bc51366ba87fef13a00c135dc6609
decent <danielekwuazi@gmail.com>
2017-09-19 20:49:30 +0700

    button style

--- src/components/Meetup/Edit/EditMeetup.vue
+++ src/components/Meetup/Edit/EditMeetup.vue
@@ -1,5 +1,7 @@
 <template>
     <v-dialog width="350px" persistent>
-        <v-btn float accent slot="activator"></v-btn>
+        <v-btn fab accent slot="activator">
+            <v-icon>edit</v-icon>
+        </v-btn>
     </v-dialog>
 </template>
