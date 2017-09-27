a6f32fd4853f4611a215e0a1cd26fdbde42337c2
decent <danielekwuazi@gmail.com>
2017-09-27 17:43:00 +0700

    date picker

--- src/components/Meetup/Edit/EditMeetupDate.vue
+++ src/components/Meetup/Edit/EditMeetupDate.vue
@@ -1,4 +1,31 @@
 <template>
-    <v-dialog persistent> 
+    <v-dialog width="350px" persistent v-model="editDialog">
+        <v-btn accent slot="activator">
+            Edit date
+        </v-btn>
+        <v-card>
+            <v-container>
+                <v-layout row wrap>
+                    <v-flex xs12>
+                        <v-card-title>Edit Meetup Date</v-card-title>
+                    </v-flex>
+                </v-layout>
+                <v-divider></v-divider>
+                <v-layout row wrap>
+                    <v-flex xs12>
+                        <v-date-picker v-model="editedDate"></v-date-picker>
+                    </v-flex>
+                </v-layout>
+                <v-divider></v-divider>
+                <v-layout row wrap>
+                    <v-flex xs12>
+                        <v-card-actions>
+                            <v-btn flat class="blue--text darken-1" @click="editDialog = false">Close</v-btn>
+                            <v-btn flat class="blue--text darken-1" @click="onSaveChanges">Save</v-btn>
+                        </v-card-actions>
+                    </v-flex>
+                </v-layout>
+            </v-container>
+        </v-card>
     </v-dialog>
 </template>
