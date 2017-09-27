c1a3323d93702e59141576dcbae52d240c377f40
decent <danielekwuazi@gmail.com>
2017-09-27 13:51:38 +0700

    validation

--- src/components/Meetup/Edit/EditMeetup.vue
+++ src/components/Meetup/Edit/EditMeetup.vue
@@ -56,6 +56,9 @@ export default {
   },
   methods: {
     onSaveChanges () {
+      if (this.editedTitle.trim() === '' || this.editedDescription.trim() === '') {
+        return
+      }
       this.editDialog = false
     }
   }
