c7c9ffeb6e601f246f858a5a59b0fd6f9f1339b5
decent <danielekwuazi@gmail.com>
2017-09-30 20:04:47 +0700

    in progress

---
+++ src/components/Meetup/Registration/Register.vue
@@ -0,0 +1,45 @@
+<template>
+    <v-dialog width="350px" persistent v-model="editDialog">
+        <v-btn accent slot="activator">
+            Register
+        </v-btn>
+        <v-card>
+            <v-container>
+                <v-layout row wrap>
+                    <v-flex xs12>
+                        <v-card-title>Register</v-card-title>
+                    </v-flex>
+                </v-layout>
+                <v-divider></v-divider>
+                <v-layout row wrap>
+                    <v-flex xs12>
+                        <v-time-picker v-model="editableTime" style="width: 100%" actions>
+                            <template scope="{save, cancel}">
+                                <v-btn class="blue--text darken-1" flat @click="editDialog = false">Close</v-btn>
+                                <v-btn class="blue--text darken-1" flat @click="onSaveChanges">Save</v-btn>
+                            </template>
+                        </v-time-picker>
+                    </v-flex>
+                </v-layout>
+            </v-container>
+        </v-card>
+    </v-dialog>
+</template>
+
+<script>
+export default {
+  props: ['meetup'],
+  data () {
+    return {
+      editDialog: false
+    }
+  },
+  methods: {
+    onSaveChanges () {
+      this.editDialog = false
+      this.$store.dispatch('updateMeetupData', {
+      })
+    }
+  }
+}
+</script>
